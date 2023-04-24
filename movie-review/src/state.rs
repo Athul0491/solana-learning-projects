51c89eccf2dbeaabe96fb7c98ce2c8975baa3a59
Athul0491 <athulrthulasidasan@gmail.com>
2023-04-24 21:33:20 +0530

    movie-review

---
+++ movie-review/src/state.rs
@@ -0,0 +1,7 @@
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieAccountState {
+    pub is_initialized: bool,
+    pub rating: u8,
+    pub title: String,
+    pub description: String  
+}
