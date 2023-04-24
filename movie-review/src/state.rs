922c16da1696b3aafd1e827e0d8751e080ae9692
Athul0491 <athulrthulasidasan@gmail.com>
2023-04-24 23:01:32 +0530

    added checks and validation

--- movie-review/src/state.rs
+++ movie-review/src/state.rs
@@ -3,5 +3,13 @@ pub struct MovieAccountState {
     pub is_initialized: bool,
     pub rating: u8,
     pub title: String,
-    pub description: String  
+    pub description: String,
+}
+
+impl Sealed for MovieAccountState {}
+
+impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
 }
